54a37bf5677c66b0bdc2c0ada833918ec3a83ae7
Luka Djordjevic <lukaluxy01@gmail.com>
2023-09-18 20:11:16 +0200

    kraj za danas

--- src/app/components/user-view/user-view.component.ts
+++ src/app/components/user-view/user-view.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { User } from 'src/app/models/user';
 
 @Component({
   selector: 'app-user-view',
   templateUrl: './user-view.component.html',
   styleUrls: ['./user-view.component.scss']
 })
-export class UserViewComponent {
+export class UserViewComponent implements OnInit {
+
+  @Input() user: User | null = null;
+
+  constructor() {}
+
+  ngOnInit(): void {
+    
+  }
 
 }
