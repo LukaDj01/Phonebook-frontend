5090870300502c4834b62402eeefbb137129fab5
Luka Djordjevic <lukaluxy01@gmail.com>
2023-09-20 17:05:21 +0200

    com2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,15 +13,13 @@ import { AppState } from './app.state';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { EffectsModule } from '@ngrx/effects';
 import { UsersEffects } from './store/user.effects';
-import { PhoneComponent } from './components/phone/phone.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     DirectoryComponent,
     UserViewComponent,
-    DirectoryItemComponent,
-    PhoneComponent
+    DirectoryItemComponent
   ],
   imports: [
     BrowserModule,
